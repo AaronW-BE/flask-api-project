82a474cf720569c52716c04612607bdf8b388666
aaronwb <zmtzwb@gmail.com>
2021-03-23 10:52:47 +0800

    add query user profile by username api

--- server/bp/user.py
+++ server/bp/user.py
@@ -1,16 +1,21 @@
-from server.utils.response import resp_success
+from server.utils.response import resp_success, resp_fail
 from flask import (
     Blueprint, flash, request, jsonify
+)
+from server.models import (
+    User, ProfileSchema
 )
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
 
-@bp.route('current-roles', methods=['GET'])
-def get_login_user_roles():
-    return resp_success({})
+@bp.route('<username>/profile', methods=['GET'])
+def get_user_profile(username):
+    # 检测用户是否存在，暂不验证参数是否合法
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return resp_fail("用户不存在")
 
-
-@bp.route('current-profile', methods=['GET'])
-def get_login_user_profile():
-    return resp_success()
+    # 返回个人信息
+    profile = ProfileSchema().dump(user.profile)
+    return resp_success(profile)
